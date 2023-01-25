71099ac03e1f54e91ed88b841247515fc367cb70
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-25 22:53:17 +0900

    update

--- python_ros_utils.py
+++ python_ros_utils.py
@@ -5,6 +5,10 @@ from std_msgs.msg import *
 from geometry_msgs.msg import *
 from sensor_msgs.msg import *
 
+# rosparam
+# usage: https://qiita.com/honeytrap15/items/550c757f2964b575883c
+import rosparam
+
 def now():
   return rospy.get_time()
 
@@ -26,6 +30,11 @@ if __name__ == '__main__':
   rospy.init_node("test")
   timer = Timer()
   r = rospy.Rate(10)
+
+  rosparam.set_param("/aaa","1")
+  get_param = rosparam.get_param("/aaa")
+  print("get param:", get_param)
+
   while not rospy.is_shutdown():
     print("start diff:", timer.st())
     print("last diff:", timer.dt())
